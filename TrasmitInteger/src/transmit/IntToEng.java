a1a5d69f754e2bc446c72c967ec99738504e4a4f
KumiKumiKumi <g12906hk+git@gmail.com>
2014-05-09 12:01:19 +0900

    ０〜１９までの数を表示できるようにした

--- TrasmitInteger/src/transmit/IntToEng.java
+++ TrasmitInteger/src/transmit/IntToEng.java
@@ -3,6 +3,13 @@ package transmit;
 import java.util.*;
 
 public class IntToEng {
+	
+	public static String[] ones_place = {"zero","one","two","three","four",
+			"five","six","seven","eight","nine"};
+	public static String[] teens = {"ten","eleven","twelve","thirteen","fourteen","fifteen",
+		"sixteen","eighteen","nineteen"};
+	public static String[] tens_place = {"twenty","thirty","forty","fifty",
+		"sixty","seventy","eighty","ninety"};
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -13,7 +20,11 @@ public class IntToEng {
 	
 	// 数値を英訳する変換するメソッド
     static String translateEng(int n) {
-    	
-        return "one";
+    	if(0<=n && n<10){
+    		return ones_place[n];
+    	}else if(n<=10 && n<20){
+    		return teens[n-10];
+    	}
+        return "null";
     }
 }
--- TrasmitInteger/test/transmit/IntToEngTest.java
+++ TrasmitInteger/test/transmit/IntToEngTest.java
@@ -12,9 +12,17 @@ public class IntToEngTest {
 	@Test
 	public void translateEngメソッドをテスト() {
 		IntToEng ite = new IntToEng();
-		String expected1 = "zero";
-		String actual1 = ite.translateEng(0);
+		String expected1 = "twenty one";
+		String actual1 = ite.translateEng(21);
 		assertThat(actual1,is(expected1));
 	}
+	/*
+	@Test
+	public void translateEngメソッドをテスト＿１桁の数() {
+		IntToEng ite = new IntToEng();
+		String expected1 = "one";
+		String actual1 = ite.translateEng(1);
+		assertThat(actual1,is(expected1));
+	}*/
 
 }
