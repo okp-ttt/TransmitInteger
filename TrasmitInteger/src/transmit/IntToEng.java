d52d4f1b8ea585fe3eafe0d3eea6936618f3eb63
g12938oy <g12938oy@m20214.tsuda.ac.jp>
2014-05-09 15:44:55 +0900

    99まで表示できるようにしました

--- TrasmitInteger/src/transmit/IntToEng.java
+++ TrasmitInteger/src/transmit/IntToEng.java
@@ -4,12 +4,12 @@ import java.util.*;
 
 public class IntToEng {
 	
-	public static String[] ones_place = {"zero","one","two","three","four",
+	public static String[] ones_place = {"one","two","three","four",
 			"five","six","seven","eight","nine"};
 	public static String[] teens = {"ten","eleven","twelve","thirteen","fourteen","fifteen",
 		"sixteen","eighteen","nineteen"};
-	public static String[] tens_place = {"twenty","thirty","forty","fifty",
-		"sixty","seventy","eighty","ninety"};
+	public static String[] tens_place = {"twenty ","thirty ","forty ","fifty ",
+		"sixty ","seventy ","eighty ","ninety "};
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -20,10 +20,15 @@ public class IntToEng {
 	
 	// 数値を英訳する変換するメソッド
     static String translateEng(int n) {
-    	if(0<=n && n<10){
+    	if(n==0) return "zero";
+    	if(n<0) return "null";
+    	if(0<n && n<10){
     		return ones_place[n];
-    	}else if(n<=10 && n<20){
+    	}else if(n<20){
     		return teens[n-10];
+    	}else if(n<100) {
+    		if(n%10==0) return tens_place[n/10-2];
+    		return tens_place[n/10-2]+ones_place[n%10-1];
     	}
         return "null";
     }
