80627ec4be91d8e802a54b8a173705c81c4babaf
g12938oy <g12938oy+git@gm.tsuda.ac.jp>
2014-05-12 14:18:05 +0900

    10万の位まで表示する

--- TrasmitInteger/test/transmit/IntToEngTest.java
+++ TrasmitInteger/test/transmit/IntToEngTest.java
@@ -12,9 +12,18 @@ public class IntToEngTest {
 	@Test
 	public void translateEngメソッドをテスト() {
 		IntToEng ite = new IntToEng();
-		String expected1 = "twenty one";
-		String actual1 = ite.translateEng(21);
+		String expected1 = "zero";
+		String expected2 = "ten";
+		String expected3 = "one hundred twenty three";
+		String expected4 = "one hundred twenty three thousand four hundred fifty six";
+		String actual1 = ite.translateEng(0);
+		String actual2 = ite.translateEng(10);
+		String actual3 = ite.translateEng(123);
+		String actual4 = ite.translateEng(123456);
 		assertThat(actual1,is(expected1));
+		assertThat(actual2,is(expected2));
+		assertThat(actual3,is(expected3));
+		assertThat(actual4,is(expected4));
 	}
 	/*
 	@Test
